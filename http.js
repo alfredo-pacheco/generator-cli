3755e321a841a4f77bee35a7b0c6280a0f67421f
alfredoPacheco <j.alfredo.pacheco@gmail.com>
2020-02-28 11:41:45 -0700

    - playing

---
+++ http.js
@@ -0,0 +1,37 @@
+require('isomorphic-fetch');
+
+const _BaseURL = 'http://localhost:4321/';
+
+const Request = async (method, endpoint, data, BaseURL) => {
+  // if (AuthService.auth == null) AuthService.fillAuthData();
+  // if (!AuthService.auth || !AuthService.auth.user) throw 'User not signed in.';
+
+  const config = {
+    method: method,
+    mode: 'cors',
+    // cache: 'no-cache',
+    headers: {
+      'Content-Type': 'application/json'
+      // Authorization: `Bearer ${AuthService.auth.user.BearerToken}`
+    },
+    body: null
+  };
+  if (['POST', 'PUT', 'DELETE'].includes(method)) config.body = JSON.stringify(data);
+  let response = await fetch((BaseURL || _BaseURL) + endpoint, config);
+  if (response) {
+    if (!response.ok) throw await response.json();
+    if (response.status == 403) console.log('Invalid Role.');
+    if (response.status == 401) throw response;
+  } else {
+    console.log('Failed to fetch. Probably server is down.');
+  }
+  try {
+    const result = await response.json();
+    return result;
+  } catch (e) {
+    return null;
+  }
+};
+
+module.exports.get = async (endpoint, baseURL) => await Request('GET', endpoint, null, baseURL);
+module.exports.post = async (endpoint, data, baseURL) => await Request('POST', endpoint, data, baseURL);
---
+++ index.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+require('yargs')
+  .scriptName('pirate-parser')
+  .usage('$0 <cmd> [args]')
+  .command(
+    'hello [name]',
+    'welcome ter yargs!',
+    yargs => {
+      yargs.positional('name', {
+        type: 'string',
+        default: 'Cambi',
+        describe: 'the name to say hello to'
+      });
+    },
+    function(argv) {
+      console.log('hello', argv.name, 'welcome to yargs!');
+    }
+  )
+  .help().argv;
+
+const args = require('yargs').argv;
+const { get, post } = require('./http');
+
+const command = args._[0];
+const name = args._[1] || null;
+
+// console.log(JSON.stringify(args, null, 3))
+// console.log(command, name)
+
+switch (command) {
+  case 'backend':
+  case 'b':
+    post('/Generator/RunBackend')
+      .then(response => {
+        console.log('Done.');
+        // console.log(response);
+      })
+      .catch(err => {
+        console.log('err');
+        console.log(err);
+      });
+    break;
+  case 'frontend':
+  case 'f':
+    console.log(`frontend ${name}`);
+    break;
+  case 'entity':
+  case 'e':
+    console.log(`entity ${name}`);
+    break;
+  case 'component':
+  case 'c':
+    console.log(`component ${name}`);
+    break;
+  case 'app':
+  case 'a':
+    console.log(`app ${name}`);
+    break;
+  default:
+    console.log(`default ${name}`);
+}
